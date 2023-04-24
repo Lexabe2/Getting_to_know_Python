29ea3b21ecca6468fd08b8dc8e6abb895e913c93
Alekseu <natash79@inbox.ru>
2023-04-24 12:34:58 +0300

    Dsgjktyf gthdfz ljvfirf

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+# Задание 1.(БАЗОВЫЕ)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+name = input("Как тебя зовут? ")
+age = int(input("Сколько тебе лет? "))
+citi = input("А в каком городе ты живешь? ")
+citi_population = input("Сколько людей в твоем городе? ")
+print(f'Тебя зовут {name}, очень приятно познакомиться, {age} какой ты большой. \n{citi} я слышал про этот город, говорят он очень красивый и в нем живет много людей аж {citi_population} человек')
 
+#Задание 2.(БАЗОВЫЕ)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+seconds = int(input("Введите секунды для перевода их в в часы, минуты и секунды: "))
+hour = int(seconds / 3600)
+minutes = int(seconds / 60)
+print(f"Результат в фомате чч:мм:сс {hour}:{minutes}:{seconds}")
 
+#Задание 3.(БАЗОВЫЕ)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+number = int(input("Введите число: "))
+summa = str(number) + str(number * 2) + str(number * 3)
+print(summa)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#Задание 4.(БАЗОВЫЕ)
+
+revenue = int(input("Введите значения выручки: "))
+costs = int(input("Введите издержеки фирмы: "))
+if revenue > costs:
+    profits = revenue - costs
+    profitability = profits / revenue
+    people = int(input("Введите кол-во людей в фирме: "))
+    profits_people = profits / people
+    print(f"Прибыль составила: {profits}, Рентабильсноть: {profitability}, Прибыль на одного сотрудника: {profits_people}")
+else: print("Стоит работать лучше!")
